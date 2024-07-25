c39f01ce93d440a47c52ee986051278887dcd027
hyungsoo1 <hyungsoo.kim@sellerhub.co.kr>
2024-07-25 23:22:17 +0900

    feat: 언어팩 적용, 회원가입 개발중

---
+++ resources/lang/jp/messages.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'welcome' => 'Welcome to our application',
+    'register' => 'Register',
+    'name' => 'Name',
+    'email' => 'Email',
+    'password' => 'Password',
+    'confirm_password' => 'Confirm Password',
+    'submit' => 'Submit',
+];
---
+++ resources/lang/ko/messages.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'welcome' => 'Welcome to our application',
+    'register' => '회원가입',
+    'name' => '이름',
+    'email' => '이메일',
+    'password' => '비밀번호',
+    'confirm_password' => '비밀번호 확인',
+    'submit' => '확인',
+];
