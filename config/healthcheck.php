d419d652c42ca301be47aaf9725bfd5163dd5ec0
root <root@ip-172-31-11-170.ap-northeast-2.compute.internal>
2024-11-10 09:45:24 +0000

    feat : ssl

---
+++ config/healthcheck.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    'path' => 'healthcheck',
+
+    'database' => true,
+
+    'redis' => false,
+
+    'horizon' => true,
+
+    'scheduler' => false,
+
+    'logging' => true,
+
+    'jobs' => [
+
+        'logging' => true,
+
+    ],
+
+];
--- routes/web.php
+++ routes/web.php
@@ -68,7 +68,9 @@ use App\Http\Controllers\Admin\LoginController;
 use App\Http\Controllers\Admin\OrderController;
 use Illuminate\Support\Facades\Artisan;
 
-
+if (env('APP_ENV') == 'production') {
+    \Illuminate\Support\Facades\URL::forceScheme('https');
+}
 ###################### 미인증 페이지 START ###########################
 
 Artisan::call('storage:link'); //프로덕션에서 심볼릭 오류가 있어서 추가함,,, ㅠㅠ
