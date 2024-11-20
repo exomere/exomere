1b613c544c2de9a7737f013100378bb3021ac390
happyoley <brdsouvenir@gmail.com>
2024-11-20 15:42:38 +0700

    feat : 1

--- app/Http/Controllers/API/OnPlatController.php
+++ app/Http/Controllers/API/OnPlatController.php
@@ -19,20 +19,11 @@ class OnPlatController extends Exomere
     public $api_path;
 
     public function __construct() {
-        
-      
 
-        if(env('APP_ENV') == 'production'){
-            $this->store_id = 29151;
-            $this->api_path = "https://pro-api.pay-onplat.com";
+        // $this->store_id = 29151;
 
-            // $this->store_id = 30693;
-            // $this->api_path = "https://pro-api.pay-onplat.com";
-            
-        }else{
             $this->store_id = 30693;
             $this->api_path = "https://pro-api.pay-onplat.com";
-        }
 
         $pgInfo = Http::withHeaders([
             'Charset' =>  'UTF-8',
