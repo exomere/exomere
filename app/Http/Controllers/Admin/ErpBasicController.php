e5384a1716c374c87ffe27f3ca37a8c8d196e26f
happyoley <brdsouvenir@gmail.com>
2024-11-22 15:22:05 +0700

    feat : 노출조건 변경

--- app/Http/Controllers/Admin/ErpBasicController.php
+++ app/Http/Controllers/Admin/ErpBasicController.php
@@ -208,7 +208,7 @@ class ErpBasicController extends Exomere
         $limitPage = $this->getPageLimit();
         $page = $request->get('page') ?? 1;
 
-        $query = ExItem::where('is_active','Y');
+        $query = ExItem::whereNotNull('id');
 
         // 검색어가 있을 경우 쿼리에 필터 추가
         if ($request->has('search_text') && $request->get('search_text') !== '') {
