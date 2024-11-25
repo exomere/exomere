14a7600ae4c30f0b97bc7d09f07b17aeb5527b5c
happyoley <brdsouvenir@gmail.com>
2024-11-25 11:54:51 +0700

    feat : 로그인시 이전페이지로 이동

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -16,8 +16,12 @@ class LoginController extends Exomere
      * @return Renderable
      */
     public function show()
-    {
-        return view('auth.login');
+    {        
+        $back_url = url()->previous();
+        $datas = [
+            'back_url' =>$back_url ?? "/",
+        ];
+        return view('auth.login')->with($datas);
     }
 
     public function login(Request $request)
@@ -26,7 +30,7 @@ class LoginController extends Exomere
         $id = $request->id ?? null;
 
         $userInfo = ExMember::where('member_id', $id)->where("member_pw", DB::raw("UPPER(SHA1(UNHEX(SHA1('".env('LOGIN_KEY'). $pwd . "'))))"))->first();
-
+        
         if ($userInfo) {
             $request->session()->regenerate();
             $request->session()->put('member_seq', $userInfo->id ?? '');
@@ -37,8 +41,12 @@ class LoginController extends Exomere
             $request->session()->put('member_level', $userInfo->member_level ?? '');
             $request->session()->put('site_code', $userInfo->site_code ?? '');
             auth()->login($userInfo);
-
-            return redirect('/management/dashboard');
+            if($request->back_url){
+                return redirect($request->back_url);
+            }else{
+                return redirect('/management/dashboard');
+            }
+            
             
         } else {
             return redirect('/login');
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -49,6 +49,7 @@
     <main class="form-signin text-center" style="background-color: #fff;border-radius: 25px">
         <form id="formAuthentication" action="{{route('login.perform')}}" method="POST">
             @csrf
+            <input type='hidden' name='back_url' value="{{$back_url}}">
             <a href="/">
                 <img class="mb-4" src="{{asset('img/logo.svg')}}" alt="" width="172"
                      height="57">
