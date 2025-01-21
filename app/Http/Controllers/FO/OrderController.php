50f95cecd99ab7ad10e2e638a810b171244347c0
happyoley <brdsouvenir@gmail.com>
2025-01-21 15:01:51 +0700

    feat : error내용 수정

--- app/Http/Controllers/FO/OrderController.php
+++ app/Http/Controllers/FO/OrderController.php
@@ -220,16 +220,16 @@ class OrderController extends Exomere
             }
 
             if(isset($request->card_name)){
-                $card_info[0]['card_company'] = $request->card_company ?? null;
-                $card_info[0]['card_name'] = $request->card_name ?? null;
-                $card_info[0]['card_number'] = $request->card_number ?? null;
-                $card_info[0]['card_payment_price'] = $request->card_payment_price ?? null;
-                $card_info[0]['card_month_plan'] = $request->card_month_plan ?? null;
-                $card_info[0]['card_year_month'] = $request->card_year_month ?? null;
-                $card_info[0]['card_approval_number'] = $request->card_approval_number ?? null;
-                $card_info[0]['card_approval_name'] = $request->card_approval_name ?? null;
-                $card_info[0]['card_approval_date'] = $request->card_approval_date ?? null;
-                $card_info[0]['card_password'] = $request->card_password ?? null;
+                $card_info_in_array[0]['card_company'] = $request->card_company ?? null;
+                $card_info_in_array[0]['card_name'] = $request->card_name ?? null;
+                $card_info_in_array[0]['card_number'] = $request->card_number ?? null;
+                $card_info_in_array[0]['card_payment_price'] = $request->card_payment_price ?? null;
+                $card_info_in_array[0]['card_month_plan'] = $request->card_month_plan ?? null;
+                $card_info_in_array[0]['card_year_month'] = $request->card_year_month ?? null;
+                $card_info_in_array[0]['card_approval_number'] = $request->card_approval_number ?? null;
+                $card_info_in_array[0]['card_approval_name'] = $request->card_approval_name ?? null;
+                $card_info_in_array[0]['card_approval_date'] = $request->card_approval_date ?? null;
+                $card_info_in_array[0]['card_password'] = $request->card_password ?? null;
             }
             /* 카드결제 */
         }else{
@@ -281,7 +281,7 @@ class OrderController extends Exomere
             "account_payment" => $account_payment ?? 0,
             "is_approval" => $is_approval ?? 'N',
             "item_info" => json_encode($item_array) ?? [],
-            "card_info" => json_encode($card_info) ?? [],
+            "card_info" => json_encode($card_info_in_array) ?? [],
             "account_info" => json_encode($account_info) ?? [],
             "order_date" => date("Y-m-d H:i:s"),
             "reg_name" => "F/O 본인결제",
