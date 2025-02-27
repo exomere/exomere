f63cf121ce6df2f5f1ddb90b5dbd4002952ece82
happyoley <brdsouvenir@gmail.com>
2025-02-27 18:40:56 +0900

    feat : FC1 변경 오류 수정

--- resources/views/pages/erp/member/register.blade.php
+++ resources/views/pages/erp/member/register.blade.php
@@ -107,7 +107,7 @@
                     <option @isset($member->member_position) @if($member->member_position == "대리점")  selected  @endif @endisset value="대리점">대리점</option>
                   @endif
                   <option @isset($member->member_position) @if($member->member_position == "총판")  selected  @endif @endisset value="총판">FC</option>
-                  <option @isset($member->member_position) @if($member->member_position == "총판1")  selected  @endif @endisset value="총판">FC1</option>
+                  <option @isset($member->member_position) @if($member->member_position == "총판1")  selected  @endif @endisset value="총판1">FC1</option>
                   <option @isset($member->member_position) @if($member->member_position == "우수총판")  selected  @endif @endisset value="우수총판">우수FC</option>
                   <option @isset($member->member_position) @if($member->member_position == "최우수총판")  selected  @endif @endisset value="최우수총판">최우수FC</option>
                 </select>
