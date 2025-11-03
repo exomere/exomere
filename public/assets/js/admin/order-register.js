703c5c4b90a49bc2d40fdd00912cfe1b4f9d5b1b
happyoley <brdsouvenir@gmail.com>
2025-11-03 10:18:11 +0900

    feat : 포인트 오류수정

--- public/assets/js/admin/order-register.js
+++ public/assets/js/admin/order-register.js
@@ -317,7 +317,7 @@ var orderReg = {
     formSave: function(){
         var point_payment = orderReg.removeComma($("#point_payment").val());
         var total = ( orderReg.removeComma($("#total_amount").val()) + orderReg.removeComma(($("#delivery_amount").val()) ));
-        var payment_amount = orderReg.removeComma($("#payment_amount").val());
+        var payment_amount = orderReg.removeComma($("#payment_amount").val()) + point_payment;
         
         if(total != payment_amount){
             alert('주문금액과 결제금액이 다릅니다.');
